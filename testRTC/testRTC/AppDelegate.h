f3873eb1ba8f2723396a9c8f6adb79d891385244
zengdan <zengdan@qq.com>
2020-09-09 15:22:48 +0800

    first submit

---
+++ testRTC/testRTC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testRTC
+//
+//  Created by DongDong on 2020/9/4.
+//  Copyright © 2020 AnhuiYinren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ testRTC/testRTC/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  testRTC
+//
+//  Created by DongDong on 2020/9/4.
+//  Copyright © 2020 AnhuiYinren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
